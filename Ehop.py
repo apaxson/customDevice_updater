cfdb1ed74c71890c9d4104d9c11f9c782c7f461e
Aaron Paxson <apax@extrahop.com>
2016-11-14 11:52:20 -0500

    Fixing crazy wierd git issue.  Pay no attention to me

--- Ehop.py
+++ Ehop.py
@@ -19,9 +19,6 @@ class Ehop(object):
 
         resp = conn.getresponse()
 
-<<<<<<< fc41b0098ea76219cf5d8a16754416fc76bb1bad
-=======
         if resp.status >= 300:
             raise ValueError('Non-200 status code from API request', resp.status, resp.reason, resp.read())
->>>>>>> Removed SSL Cert check and changed return of api_request() to full response object.
         return resp
